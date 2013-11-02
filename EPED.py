04ab7d1d38d394bc5a45da35262bb4d595c6cb17
Evan Davis <emd@mit.edu>
2013-11-02 16:51:59 -0700

    Updated EPED diamagnetic stabilization model to use the exact coefficients used in EPED1.6. However, have *slightly* different definitions of omega_star_i that result in O(1) corrections.

--- EPED.py
+++ EPED.py
@@ -42,23 +42,9 @@ def omega_star_eff(g, n, kappa, a, Z = 1, A = 2, density_norm = 1e20):
     This is the same model of diamagnetic stabilization used in EPED1.6.
 
     '''
-    # The strength of the diamagnetic stabilization in regions 1 and 2
-    # are *fractions* of the maximum ion diamagnetic stabilization. 
-    # Further, we care about the strength of this stabilization as a 
-    # function of the toroidal mode number, n. Thus, we want to 
-    # determine the rate m_max at which the ion diamagnetic stabilization 
-    # changes with n, e.g. m_max = d(omega_{*i,max})/dn. 
-    # 
-    # Now, as omega_{*i,max} ~ n, we can simply compute
-    # m_max = (omega_{*i,max}(n=1) - omega_{*i,max}(n=0)) / (1 - 0)
-    # i.e. m_max = omega_{*i,max}(n=1)
-    omega_n1 = ba.elm_pb.omega_star_i(g, np.array([1]), kappa, a, Z = Z, 
-                                      A = A, density_norm = density_norm) 
-    m_max = np.amax(omega_n1, axis = 1) 
-    
-    # slopes of regions 1 and 2 
-    m1 = 0.735026371695 * m_max 
-    m2 = 0.121157912906 * m_max 
+    omega_star_i = ba.elm_pb.omega_star_i(g, n, kappa, a, Z = Z, A = A,  
+                                          density_norm = density_norm) 
+    omega_star_i = np.amax(omega_star_i, axis = 1) 
 
     psi = ba.grid.grid2psi(g, vector = True)
     q = ba.grid.grid2q(g)
@@ -73,36 +59,11 @@ def omega_star_eff(g, n, kappa, a, Z = 1, A = 2, density_norm = 1e20):
     n_crit = 27.7 / q_95
 
     omega = np.zeros(len(n))
-    omega_crit = m1 * n_crit
 
     for i in range(len(n)):
         if n[i] < n_crit:
-            omega[i] = m1 * n[i]
+            omega[i] = np.sqrt(0.5) * omega_star_i[i] 
         else:
-            omega[i] = omega_crit + (m2 * (n[i] - n_crit))
+            omega[i] = np.sqrt(0.5) * (n_crit + 0.168 * (n - n_crit)) * omega_star_i[i] / n[i]
 
     return omega
-
-
-# Computes the slopes for the bilinear fitting function described in:
-#
-#        Snyder et al. Nucl. Fusion 51 (2011) 103016
-#
-# and prints the slopes to screen. 
-if __name__ == '__main__':
-    import numpy as np
-   
-    # omega_{*eff} curve from Fig. 4 
-    n = np.array([0, 14, 40])
-    omega = np.array([0, 0.57647, 0.75294])
-    
-    m1 = (omega[1] - omega[0]) / (n[1] - n[0])
-    m2 = (omega[2] - omega[1]) / (n[2] - n[1])
-    
-    # omega_{*max} curve from Fig. 4 
-    n = np.array([0, 17.8352])
-    omega = np.array([0, 0.999134])
-    m_max = (omega[1] - omega[0]) / (n[1] - n[0])
-
-    print 'slope in region 1: m1 = ' + str(m1 / m_max) + ' m_max'
-    print 'slope in region 2: m2 = ' + str(m2 / m_max) + ' m_max'
