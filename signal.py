9209efc1aa3daf8e2ccdccf69c8b244ed2a25c37
Evan Davis <emd@mit.edu>
2013-11-07 17:04:10 -0800

    Added routine to signal.py to calculate the frequency of a signal. Rudimentary routine that needs work...

--- signal.py
+++ signal.py
@@ -1,5 +1,7 @@
 '''Analyze BOUT++ output signals.'''
 
+import numpy as np
+import matplotlib.pyplot as plt
 
 def moment_xyzt(s):
     '''Calculate the moments of a 4d signal (t,x,y,z).
@@ -19,8 +21,6 @@ def moment_xyzt(s):
     NOTE: One can reconstruct the full signal via s = s_dc + s_ac
 
     '''
-    import numpy as np
-
     # Take the RMS of the signal
     s_rms = np.sqrt(np.mean((s ** 2), axis = 3))
 
@@ -45,9 +45,6 @@ def growth_rate(s, t = None, plot = True):
     gamma -- a 1d array of growth rate [\omega_A] as a function of time
 
     '''
-    import numpy as np
-    import matplotlib.pyplot as plt
-
     # Calculate the max value at each point in time
     s = np.amax(np.amax(s, axis = 1), axis = 1)
 
@@ -82,3 +79,53 @@ def growth_rate(s, t = None, plot = True):
 
     return gamma
 
+
+def omega(s, t, t0):
+    '''Compute the angular frequency of a 1d signal s = s(t,x,y,z).
+
+    Parameters:
+    s -- a 1d signal s(t,x,y,z)
+    t -- a 1d array with time values corresponding to the signal s
+    t0 -- a scalar; time values below this value are ignored
+
+    Returns:
+    omega -- the angular frequency [\omega_A] of the signal s.
+
+    The premise of this routine is to find the peaks in the signal
+    (assumed to be varying as exp(i * omega * t)), and then 
+    calculate the period T by realizing the distance between the
+    extrema will be T / 2
+
+    Future Improvements:
+    (1) Somehow integrate with calculation of growth rate
+    (2) Use something more accurate/elegant, e.g. Fourier Transforms
+    (3) Handling of signals of many dimensions
+    (4) The *sign* of the frequency (e.g. electron or ion direction)
+    (5) Normalized vs physical units as an option 
+
+    '''
+    from scipy.signal import argrelextrema
+
+    ## Plot signal so user can select appropriate range to find extrema
+    #plt.plot(t, s)
+    #plt.xlabel('$t$ [$\\tau_A$]')
+    #plt.ylabel('Signal $s(x_0, t)$')
+    #plt.show()
+
+    # obtain minimum time, above which we will search for extrema,
+    # and convert this minimum time into an appropriate signal index
+    #t0 = float(raw_input('t0 = '))
+    ind = np.where(np.abs(t - t0) == np.min(np.abs(t - t0)))
+    t = t[ind[0]:]
+    s = s[ind[0]:]
+
+    maxind = argrelextrema(s, np.greater)
+    minind = argrelextrema(s, np.less)
+
+    tmax = t[maxind]
+    tmin = t[minind]
+
+    dt = np.abs(tmax[-1] - tmin[-1])
+    T = 2 * dt
+
+    return 2 * np.pi / T 
